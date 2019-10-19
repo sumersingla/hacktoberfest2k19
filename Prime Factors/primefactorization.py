fa2d4cc74f4632f44ad65c16a7af0d32d8af60e1
sumersingla <55348133+sumersingla@users.noreply.github.com>
2019-10-20 02:17:00 +0530

    Updated Prime Factorization

--- Prime Factors/primefactorization.py
+++ Prime Factors/primefactorization.py
@@ -1,14 +1,14 @@
-import math 
-def is_prime(n): 
-    if n <= 1: 
-        return False
-    max_div = math.floor(math.sqrt(n)) 
-    for i in range(2, 1 + max_div): 
-        if n % i == 0: 
-            return False
-    return True
-k = int(input("Enter a number:"))
-print("Prime numbers are:")
-for n in range(1,k): 
-    x = is_prime(n) 
-    print(x,end = '')
+n=int(input("enter an integer:"))
+print("factors are:")
+i=1
+while (i<=n):
+    k=0
+    if(n%i==0):
+        j=1
+        while(j<=i):
+                if(i%j==0):
+                        k=k+1
+                    j=j+1
+                if(k==2):
+                    print(i)
+            i=i+1
