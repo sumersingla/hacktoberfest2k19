b8e11e67db4c17ef9bb209908a39bdd9bed6ba12
sumersingla <55348133+sumersingla@users.noreply.github.com>
2019-10-20 01:12:32 +0530

    Updated Code
    While loop was needed. Debugged other errors.

--- Armstrong Number/ArmstrongNumber.py
+++ Armstrong Number/ArmstrongNumber.py
@@ -1,6 +1,10 @@
-number, new_number  = input("Enter a number to check if it is an Armstrong Number: "), 0
-for i in number:
-    new_number += int(i) ** 3
+number= int(input("Enter a number to check if it is an Armstrong Number: "))
+new_number=0
+temp=number
+while temp > 0:
+   digit = temp % 10
+   new_number += digit ** 3
+   temp //= 10
 if number == new_number:
     print("The number is an Armstrong number")
 else:
